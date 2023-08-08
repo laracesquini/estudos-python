4a51a47ebbb282eb23a3a2521679f38d72bd4fe7
laracesquini <laracesquini@gmail.com>
2023-08-08 15:18:45 -0300

    Conceitos básicos

---
+++ teoria/teoria001.py
@@ -0,0 +1,15 @@
+#comandos básicos
+print('Olá Mundo!')
+print(7 + 4) #printa 11 - soma
+print('7' + '4') #printa 74 - concatena
+print('Olá', 5) #printa Olá 5, nesse caso o + não funcionaria
+
+#variáveis - para o python, toda variável é um objeto
+nome = 'Lara'
+idade = 19
+print(nome, idade)
+
+#perguntar para o usuário
+nome1 = input('Qual o seu nome? ') 
+idade1 = input('Qual a sua idade? ')
+print(nome1, idade1)
