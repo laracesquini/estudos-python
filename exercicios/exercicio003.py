de5120ce8f553f3069df419fbcb83e98c5628009
laracesquini <laracesquini@gmail.com>
2023-08-09 21:14:01 -0300

    Conceitos básicos

--- exercicios/exercicio003.py
+++ exercicios/exercicio003.py
@@ -1,3 +1,7 @@
-num1 = input('Digite um número: ')
+num1 = input('Digite um número: ') 
 num2 = input('Digite outro número: ')
-print('A soma é {}{}'.format(num1, num2))
+print('A soma é ' + num1 + num2) #printa 105, isso ocorre poruqe o input lê a entrada com uma string
+
+n1 = int(input('Digite um número: ')) #agora tudo que estiver dentro do parenteses vai ser considerado inteiro
+n2 = int(input('Digite outro número: '))
+print('A soma é ', n1 + n2)
