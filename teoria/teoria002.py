5f1a4c80296f68009740cda0800f7e7437efe327
laracesquini <laracesquini@gmail.com>
2023-08-09 21:14:13 -0300

    Tipos primitivos

---
+++ teoria/teoria002.py
@@ -0,0 +1,6 @@
+#tipos primitivos
+print('A soma vale {}'.format(10)) #outro jeito de usar o print
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print('A soma entre {} e {} vale {}'.format(n1, n2, n1+n2))
+
---
+++ teoria/teoria003.py
@@ -0,0 +1,2 @@
+n = input('Digite um valor: ')
+print(n.isnumeric()) #esse método diz se é possível converter esse valor em númerico (no caso)
