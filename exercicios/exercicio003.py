d5b11cf45de8ad82298612a43618a9ba7bb925e6
laracesquini <laracesquini@gmail.com>
2023-08-09 21:28:41 -0300

    Tipos primitivos

--- exercicios/exercicio003.py
+++ exercicios/exercicio003.py
@@ -2,6 +2,6 @@ num1 = input('Digite um número: ')
 num2 = input('Digite outro número: ')
 print('A soma é ' + num1 + num2) #printa 105, isso ocorre porque o input lê a entrada como uma string
 
-n1 = int(input('Digite um número: ')) #agora tudo que estiver dentro do parenteses vai ser considerado inteiro
+n1 = int(input('Digite um número: ')) #agora tudo que estiver dentro do parenteses vai ser considerado inteiro.
 n2 = int(input('Digite outro número: '))
 print('A soma é ', n1 + n2)
