6e1fb1a6e9789a9fc7e9e81bbc1da67937ea0b03
laracesquini <laracesquini@gmail.com>
2023-08-19 09:49:49 -0300

    Operadores aritméticos

---
+++ teoria/teoria004.py
@@ -0,0 +1,28 @@
+#operadores aritméticos
+#(+) - adição 
+#(-) - subtração
+#(*) - multiplicação
+#(/) - divisão
+#(**) - potência
+#(//) - divisão inteira
+#(%) - resto da divisão
+
+#ordem de precedência
+#1 - Parenteses
+#2 - **
+#3 - *, /, //, %
+#4 - +, -
+
+5+2 == 7
+5-2 == 3
+5*2 == 10
+5/2 == 2.5
+5**2 == 25
+5//2 == 2 
+5%2 == 1
+
+5+3*2 == 11
+3*4 + 4**2 == 31
+3*(5+4)**2 == 243
+
+#posso usar pow(4, 3) - 4 elevado a 3. Perde a ordem de precedência
